ae7dae0a01508a6bb316fa8efdb2f4d5e1d6857c
Romain Pouclet <romain.pouclet@gmail.com>
2016-04-14 17:15:22 -0400

    TouchableButton → ExtendedButton

--- Taylor/ExtendedButton.swift
+++ Taylor/ExtendedButton.swift
@@ -8,21 +8,19 @@
 
 import UIKit
 
-public class TouchableButton: UIButton
+public class ExtendedButton: UIButton
 {
     var minimalTapableSize = CGSize.minimalTapableSize()
 
     override public func pointInside(point: CGPoint, withEvent event: UIEvent?) -> Bool
     {
-        let yOffset: CGFloat = max(0, minimalTapableSize.width - frame.height)
-        let xOffset: CGFloat = max(0, minimalTapableSize.height - frame.width)
-        if(yOffset > 0 || xOffset > 0)
-        {
+        let yOffset = max(0, minimalTapableSize.width - frame.height)
+        let xOffset = max(0, minimalTapableSize.height - frame.width)
+
+        if(yOffset > 0 || xOffset > 0) {
             return bounds.insetBy(dx: -xOffset / 2, dy: -yOffset / 2).contains(point)
         }
-        else
-        {
-            return super.pointInside(point, withEvent: event)
-        }
+
+        return super.pointInside(point, withEvent: event)
     }
 }
