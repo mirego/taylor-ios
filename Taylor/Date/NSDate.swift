8cd7b3bde7f29e6e27a0d1a3a33fb6df8a59fba1
Luc Dion <ldion@mirego.com>
2016-07-19 16:10:47 -0400

    Update method parameters name * NSDate.isEqual(to: NSDate, unitGranularity: NSCalendarUnit = .Second) -> Bool * NSDate.isEarlier(than: NSDate, unitGranularity: NSCalendarUnit = .Second) -> Bool * NSDate.isLater(than: NSDate, unitGranularity: NSCalendarUnit = .Second) -> Bool

--- Taylor/Date/NSDate.swift
+++ Taylor/Date/NSDate.swift
@@ -39,46 +39,46 @@ extension NSDate
      */
     public func isSameDay(anotherDate: NSDate) -> Bool
     {
-        return isEqual(anotherDate, unitGranularity: .Day)
+        return isEqual(to: anotherDate, unitGranularity: .Day)
     }
 
     /**
      Returns true if the date is equal to the specified date using the specified NSCalendarUnit.
 
-     - parameter anotherDate:     The second date.
+     - parameter to:              The comparison other date.
      - parameter unitGranularity: Optional NSCalendarUnit. Default is Second. 
 
      - returns: Returns true if the date is equal to the specified date using the specified NSCalendarUnit
      */
-    public func isEqual(anotherDate: NSDate, unitGranularity: NSCalendarUnit = .Second) -> Bool
+    public func isEqual(to toDate: NSDate, unitGranularity: NSCalendarUnit = .Second) -> Bool
     {
-        return NSCalendar.currentCalendar().compareDate(self, toDate: anotherDate, toUnitGranularity: unitGranularity) == NSComparisonResult.OrderedSame
+        return NSCalendar.currentCalendar().compareDate(self, toDate: toDate, toUnitGranularity: unitGranularity) == NSComparisonResult.OrderedSame
     }
 
     /**
      Returns true if the date is earlier than the specified date using the specified NSCalendarUnit.
 
-     - parameter anotherDate:     The second date.
+     - parameter than:            The comparison other date.
      - parameter unitGranularity: Optional NSCalendarUnit. Default is Second.
 
      - returns: Returns true if the date is earlier than the specified date using the specified NSCalendarUnit
      */
-    public func isEarlier(anotherDate: NSDate, unitGranularity: NSCalendarUnit = .Second) -> Bool
+    public func isEarlier(than thanDate: NSDate, unitGranularity: NSCalendarUnit = .Second) -> Bool
     {
-        return NSCalendar.currentCalendar().compareDate(self, toDate: anotherDate, toUnitGranularity: unitGranularity) == NSComparisonResult.OrderedAscending
+        return NSCalendar.currentCalendar().compareDate(self, toDate: thanDate, toUnitGranularity: unitGranularity) == NSComparisonResult.OrderedAscending
     }
 
     /**
      Returns true if the date is later than the specified date using the specified NSCalendarUnit.
 
-     - parameter anotherDate:     The second date.
+     - parameter than:            The comparison other date.
      - parameter unitGranularity: Optional NSCalendarUnit. Default is Second.
 
      - returns: Returns true if the date is later than the specified date using the specified NSCalendarUnit
      */
-    public func isLater(anotherDate: NSDate, unitGranularity: NSCalendarUnit = .Second) -> Bool
+    public func isLater(than thanDate: NSDate, unitGranularity: NSCalendarUnit = .Second) -> Bool
     {
-        return NSCalendar.currentCalendar().compareDate(self, toDate: anotherDate, toUnitGranularity: unitGranularity) == NSComparisonResult.OrderedDescending
+        return NSCalendar.currentCalendar().compareDate(self, toDate: thanDate, toUnitGranularity: unitGranularity) == NSComparisonResult.OrderedDescending
     }
 
     public func daysBetween(toDate toDate: NSDate) -> Int
--- TaylorTests/NSDateTest.swift
+++ TaylorTests/NSDateTest.swift
@@ -1,8 +1,11 @@
+
+
+
 //
-//  TextLabelTests.swift
+//  NSDateTests.swift
 //  Taylor
 //
-//  Created by Luc Dion on 2016-05-11.
+//  Created by Luc Dion on 2016-07-11.
 //  Copyright © 2016 Mirego. All rights reserved.
 //
 
@@ -10,25 +13,44 @@ import UIKit
 import XCTest
 import Taylor
 
-class LabelTests: XCTestCase {
+class NSDateTests: XCTestCase {
 
-    func testSizeThatFits() {
-        let text = "A very very very long string that is wider than the specified width"
-        let sizeFits = CGSize(width: 40, height: .max)
+    func testEarlierAndLater() {
+        let now = NSDate()
+        let laterDate = NSDate(timeIntervalSinceNow: 1000)
+        let earlierDate = NSDate(timeIntervalSince1970: 1)
+        XCTAssertTrue(laterDate.isLater(than: now))
+        XCTAssertTrue(earlierDate.isEarlier(than: now))
 
-        let singleLineTextLabel = Label()
-        singleLineTextLabel.text = text
-
-        let singleLineTextLabelSize = singleLineTextLabel.sizeThatFits(sizeFits)
-        XCTAssertLessThanOrEqual(singleLineTextLabelSize.width, sizeFits.width)
+        XCTAssertFalse(now.isLater(than: now))
+        XCTAssertFalse(now.isEarlier(than: now))
 
 
-        let multiLineTextLabel = Label()
-        multiLineTextLabel.numberOfLines = 0
-        multiLineTextLabel.lineBreakMode = .ByWordWrapping
-        multiLineTextLabel.text = text
+        // Test unitGranularity
+        let oneDayLaterDate = now.addDaysToDate(1)
+        // Is at least one DAY later?
+        XCTAssertTrue(oneDayLaterDate.isLater(than: now, unitGranularity: .Day))
+        // Is at least one MONTH later?
+        XCTAssertFalse(oneDayLaterDate.isLater(than: now, unitGranularity: .Month))
+        // Is at least one YEAR later?
+        XCTAssertFalse(oneDayLaterDate.isLater(than: now, unitGranularity: .Year))
 
-        let multiLineTextLabelSize = multiLineTextLabel.sizeThatFits(sizeFits)
-        XCTAssertLessThanOrEqual(multiLineTextLabelSize.width, sizeFits.width)
+        let oneDayEarlierDate = now.addDaysToDate(-1)
+        // Is at least one DAY earlier?
+        XCTAssertTrue(oneDayEarlierDate.isEarlier(than: now, unitGranularity: .Day))
+        // Is at least one MONTH earlier?
+        XCTAssertFalse(oneDayEarlierDate.isEarlier(than: now, unitGranularity: .Month))
+        // Is at least one YEAR earlier?
+        XCTAssertFalse(oneDayEarlierDate.isEarlier(than: now, unitGranularity: .Year))
+
+
+        // isEqual
+        XCTAssertTrue(now.isEqual(now))
+        // Is same DAY?
+        XCTAssertFalse(oneDayLaterDate.isEqual(to: now, unitGranularity: .Day))
+        // Is same MONTH?
+        XCTAssertTrue(oneDayLaterDate.isEqual(to: now, unitGranularity: .Month))
+        // Is same YEAR?
+        XCTAssertTrue(oneDayLaterDate.isEqual(to: now, unitGranularity: .Year))
     }
 }
